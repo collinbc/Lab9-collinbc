52c547176ad496a35a856995500f84b0656ad09c
Brian Collins <collinbc@rose-hulman.edu>
2011-05-02 23:13:09 -0400

    'task 4 complete I think'

--- ChatBot-Team04/features/step_definitions/cli_steps.rb
+++ ChatBot-Team04/features/step_definitions/cli_steps.rb
@@ -1,6 +1,15 @@
 Given /^the application is running$/ do
 run_interactive(unescape("ruby chatbot.rb"))
 end
+
+Given /^I see "([^""]*)"$/ do |text|
+assert_partial_output(text)
+end
+
 Then /^I should see "([^""]*)"$/ do |text|
 assert_partial_output(text)
+end
+
+When /^I type "([^"]*)" and press Enter$/ do |input|
+type(input)
 end
